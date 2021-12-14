45e95106ffadba6e73bb81e7f210d1306849d92a
Leonardo Lima Coelho <ryszol@protonmail.com>
2021-12-14 19:20:21 -0400

    Indentation

--- environment.sh
+++ environment.sh
@@ -10,15 +10,15 @@ sudo apt update
 # Install curl
 sudo apt install curl -y &&
 
-  # Install dependencies nvm
-  sudo apt install build-essential libssl-dev -y &&
+# Install dependencies nvm
+sudo apt install build-essential libssl-dev -y &&
 
-  # Install NVM
-  if [ -d "$NVM_DIR" ]; then
-    echo "nvm is already installed"
-  else
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
-  fi
+# Install NVM
+if [ -d "$NVM_DIR" ]; then
+  echo "nvm is already installed"
+else
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+fi
 
 if [ -f ~/.bashrc ]; then
   source $HOME/.bashrc
@@ -33,17 +33,19 @@ fi
 # Install Node.js LTS
 nvm install --lts &&
 
-  # Install Snapd
-  sudo apt install snapd snapd-xdg-open &&
+# Install Snapd
+sudo apt install snapd snapd-xdg-open &&
 
-  # Install chromium
-  sudo snap install chromium &&
+# Install chromium
+sudo snap install chromium &&
 
-  # Install Visual Studio Code
-  sudo snap install code --classic &&
+# Install Visual Studio Code
+sudo snap install code --classic &&
 
-  # Install Insomnia
-  sudo snap install insomnia &&
+# Install Insomnia
+sudo snap install insomnia &&
 
-  # Database manager Beekeeper Studio
-  sudo snap install beekeeper-studio
+# Database manager Beekeeper Studio
+sudo snap install beekeeper-studio
+
+echo 'End'
